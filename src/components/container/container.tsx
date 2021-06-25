c882eeb94abfb104c48bafbcf3f8cbd526fca751
m.orlov <zogreptile@gmail.com>
2021-06-25 20:23:14 +0700

    Container js -> ts

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import classname from '../../helpers/classname';
 
-const Container = (props) => (
+interface IContainerProps {
+  mix?: string;
+  mods?: Record<string, string | boolean>;
+}
+
+const Container: React.FC<IContainerProps> = (props) => (
   <div className={classname('container', props)}>
     {props.children}
   </div>
 );
-
-Container.propTypes = {
-  children: PropTypes.node,
-};
 
 export default Container;
